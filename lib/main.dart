2163bee0644740e7f34c006b584ad8a2e94bb2da
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-01-04 00:11:11 +0200

    styling the transactions

--- lib/main.dart
+++ lib/main.dart
@@ -49,11 +49,40 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Text(tx.title),
-              );
-            }).toList(),
+            children: transactions.map(
+              (tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          tx.amount.toString(),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tx.title),
+                          Text(
+                            tx.date.toString(),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ).toList(),
           )
         ],
       ),
